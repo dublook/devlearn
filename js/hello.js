e1d1ff3b4e419e19f13c149073e9906f0972b317
dublook <kotafutami@gmail.com>
2014-03-02 21:13:22 +0900

    sing "Titanium"

--- js/hello.js
+++ js/hello.js
@@ -1,3 +1,5 @@
 console.log('hello world!');
 console.log('good night, world!');
 console.log('Get back! Get back!');
+console.log('We all in a yellow submarine.');
+console.log('I am bulletproof, nothing to loose');
